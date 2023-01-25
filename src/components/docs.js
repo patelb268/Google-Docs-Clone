52bb4b742be1916a5d415e6a5a3ff01c29fca231
patelb268 <patel.prayag.b@gmail.com>
2023-01-25 10:45:57 -0700

    create docs component

---
+++ src/components/docs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Docs() {
+  return (
+    <div className='docs-main'>
+        <h1>Docs Clone</h1>
+
+        <button className='add-docs'>
+            Add a Document
+        </button>
+    </div>
+  )
+}
