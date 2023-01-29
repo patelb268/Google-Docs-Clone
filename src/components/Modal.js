fc35f0baad3bac81df7e1972450429e026274085
patelb268 <patel.prayag.b@gmail.com>
2023-01-29 13:34:08 -0700

    add input and button to model

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,11 +9,11 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 500,
+    height: 150,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
     boxShadow: 24,
-    p: 4,
+    p: 5,
 };
 
 export default function ModalComponent({
@@ -31,12 +31,17 @@ export default function ModalComponent({
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Text in a modal
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
+                    <input
+                        placeholder='Add the Title'
+                        className='add-input'
+                    />
+                    <div className='button-container'>
+                        <button
+                            className='add-docs'
+                        >
+                            Add
+                        </button>
+                    </div>
                 </Box>
             </Modal>
         </div>
