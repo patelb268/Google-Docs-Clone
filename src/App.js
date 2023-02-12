0e7f48a6dcf0054ff693804bb18fdbb878538456
patelb268 <patel.prayag.b@gmail.com>
2023-02-12 14:27:18 -0700

    add route to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Docs from './components/Docs';
+import EditDocs from './components/EditDocs';
+import { Routes, Route } from "react-router-dom";
 import { app, database } from './firebaseConfig';
 
 function App() {
   return (
-    <Docs database={database}/>
+    <Routes>
+      <Route path="/" element={<Docs database={database} />} />
+      <Route path="/editDocs/:id" element={<EditDocs database={database}/>} />
+    </Routes>
   );
 }
 
