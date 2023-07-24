b3a5bd72788d1d05fd7c0ed8fdc69d87f8ddb881
PRAYAG PATEL <109325051+patelb268@users.noreply.github.com>
2023-07-24 05:14:03 -0700

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,13 +7,7 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAtUvpd9ISFxHH8-1MukA2lxLCYLtWokjA",
-  authDomain: "docs-clone-8f20c.firebaseapp.com",
-  projectId: "docs-clone-8f20c",
-  storageBucket: "docs-clone-8f20c.appspot.com",
-  messagingSenderId: "959429754240",
-  appId: "1:959429754240:web:21fc6b07396def21705565",
-  measurementId: "G-SDL3B65N45"
+ //put your own configuration code
 };
 
 // Initialize Firebase
